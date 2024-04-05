750ec4a1eb9ee01c2424cd44e5717d63c33a988e
unknown <hhk70313@gmail.com>
2024-04-05 17:16:41 +0530

    update

---
+++ src/Data/QuizData.js
@@ -0,0 +1,24 @@
+export const QuizData = [
+    
+    {
+        question: "A train running at the speed of 60 km/hr crosses a pole in 9 seconds. What is the length of the train?",
+        options:[ "120", "180", "324", "150"],
+        answer: 4
+    },
+    {
+        question: "A train 240 m long passes a pole in 24 seconds. How long will it take to pass a platform 650 m long",
+        options:["65sec" ,"89sec", "100sec","150sec"],
+        answer: 1
+    },
+    {
+        question: "Look at this series: 2, 1, (1/2), (1/4), ... What number should come next?",
+        options: ["1/8", "1/16", "1/3", "2/8"],
+        answer: 1
+    },
+    {
+        question: "A can do a piece of  work in 30 days and B can do piece of in 20 days. If they work on it together how days will days be finish it? ",
+        options: ["15","12","10","11"],
+        answer:1
+    }
+   
+];
